aaf29ebc4d5a796c3a4d301302d14dc3dbdb4534
Giakox74 <118228966+Giakox74@users.noreply.github.com>
2026-03-29 21:52:22 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+const CACHE_NAME = "dsa-cache-v1";
+const urlsToCache = [
+  "./",
+  "./dashboard.html",
+  "./logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
